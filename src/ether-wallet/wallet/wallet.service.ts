a2c85cafbcf6a933c5731e45588ca8d16e736bd2
0x00 pluto <0x00pluto@gmail.com>
2023-04-04 17:52:41 +0800

    add getProviderWithConfig

--- src/ether-wallet/wallet/wallet.service.ts
+++ src/ether-wallet/wallet/wallet.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ethers } from 'ethers';
+import { ConfigService } from '../../utils/config/config.service';
 
 @Injectable()
 export class WalletService {
+  @Inject()
+  private readonly configService: ConfigService;
+
   getBalance() {
     return 'getBalance';
   }
 
   getProvider(url: string): ethers.providers.Provider {
     return new ethers.providers.JsonRpcProvider(url);
+  }
+
+  getProviderWithNetworkConfig(network: string): ethers.providers.Provider {
+    const providers = this.configService.get('providers');
+    const provider = providers[network];
+    if (!provider) {
+      return null;
+    }
+    return this.getProvider(provider.rpcUrl);
   }
 }
